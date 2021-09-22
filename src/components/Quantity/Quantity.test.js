757b838a2c33d42c04b679bc6bf491123d33b732
Viktoria Moskovka <viktoriamoskovka@Viktorias-MacBook-Pro.local>
2021-09-22 17:43:48 +0300

    numbers test

--- src/components/Quantity/Quantity.test.js
+++ src/components/Quantity/Quantity.test.js
@@ -5,7 +5,8 @@ describe('Quantity calculator', () => {
   it('should be defined', () => {
     expect(recalculate).toBeDefined()
     expect(recalculate).not.toBeNaN()
-  }),
+  })
+
     it('returns correct answer', () => {
     expect(recalculate(24, 26)).toBeCloseTo(1.17)
   })
--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import firebase from 'firebase';
+import firebase from "firebase";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKBugwUQvKPrSCjQnW7M1uAwqvh3gg5MY",
-    authDomain: "pastry-converter.firebaseapp.com",
-    databaseURL: "https://pastry-converter-default-rtdb.firebaseio.com",
-    projectId: "pastry-converter",
-    storageBucket: "pastry-converter.appspot.com",
-    messagingSenderId: "32962123233",
-    appId: "1:32962123233:web:968a5cc0b99b90a80d6ea0"
+  apiKey: "AIzaSyBKBugwUQvKPrSCjQnW7M1uAwqvh3gg5MY",
+  authDomain: "pastry-converter.firebaseapp.com",
+  databaseURL: "https://pastry-converter-default-rtdb.firebaseio.com",
+  projectId: "pastry-converter",
+  storageBucket: "pastry-converter.appspot.com",
+  messagingSenderId: "32962123233",
+  appId: "1:32962123233:web:968a5cc0b99b90a80d6ea0"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
